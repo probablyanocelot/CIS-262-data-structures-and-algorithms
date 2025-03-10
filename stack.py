6a53a2109c24caa99f30e333f8c771b7545861a7
probablyanocelot <58635071+probablyanocelot@users.noreply.github.com>
2025-03-10 11:50:00 -0500

    Create stack.py

---
+++ stack.py
@@ -0,0 +1,45 @@
+class Node:
+	def __init__(self):
+		self.data = FALSE
+		self.next = FALSE
+
+class Stack:
+	def __init__(self):
+		self.top = 0
+	
+	def isEmpty(self):
+		if not self:
+			return TRUE
+		return FALSE`
+	
+	def __len__(self):
+		pass
+		
+	def __str__(self):
+		pass
+		
+	def __iter_(self):
+		pass
+		
+	def __contains(item):
+		pass
+		
+	def __add__(self):
+		pass
+		
+	def __eq__(self):
+		pass
+		
+	
+	
+	def clear(self):
+		pass
+		
+	def peek(self):
+		pass
+		
+	def push(self, item):
+		pass
+		
+	def pop(self):
+		pass
